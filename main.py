337cb68edcc8d24b404cd62a3305c12509ce9fd1
Timothy Lock <lock.k.timothy@gmail.com>
2019-02-20 14:25:32 -0500

    Refactor
    Separate thread to support quick commands

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ motorOpen = None
 motorClose = None
 motorOpenPin = 20
 motorClosePin = 21
+desiredState = "closed"
 
 # App routes
 @app.route("/")
@@ -29,83 +30,74 @@ def state():
 
 # Helper Functions
 def setState():
+    global desiredState
     c = request.json
 
     if "active" not in c:
         return "missing \"active\" field", 400
 
     if c["active"] == "true":
-        if turnOn():
-            return "turned on", 200
-        else:
-            return "failed to turn on", 500
+        desiredState = "open"
 
     if c["active"] == "false":
-        if turnOff():
-            return "turned off", 200
-        else:
-            return "failed to turn off", 500
+        desiredState = "closed"
 
-    return "Invalid payload", 400
+    return "OK", 200
 
 def fetchState():
     return json.dumps({"is_active": str(isOpen).lower()}), 200
 
 def turnOn():
-    global isOpen
-    global inMotion
-
-    # early bypass
-    if isOpen:
-        return True
-
-    isOpen = True
-
-    if inMotion:
-        return False
-
-    t = threading.Thread(target=openBlinds)
-    t.start()
-
+    global desiredState
+    desiredState = "open"
     return True
 
 def turnOff():
-    global isOpen
-    global inMotion
-
-    # early bypass
-    if not isOpen:
-        return True
-
-    isOpen = False
-
-    if inMotion:
-        return False
-
-    t = threading.Thread(target=closeBlinds)
-    t.start()
-
+    global desiredState
+    desiredState = "closed"
     return True
 
 def openBlinds():
     global isOpen
     global inMotion
+    global motorOpen
 
     inMotion = True
     motorOpen.start(100)
     time.sleep(15)
     motorOpen.stop()
     inMotion = False
+    isOpen = True
 
 def closeBlinds():
     global isOpen
     global inMotion
+    global motorClose
 
     inMotion = True
     motorClose.start(60)
     time.sleep(45)
     motorClose.stop()
     inMotion = False
+    isOpen = False
+
+def checker():
+    global isOpen
+    global inMotion
+    global desiredState
+
+    while True:
+        time.sleep(0.5)
+
+        # Skip if the blinds are in motion
+        if inMotion:
+            continue
+
+        if desiredState == "open" and not isOpen:
+            threading.Thread(target=openBlinds).start()
+
+        if desiredState == "closed" and isOpen:
+            threading.Thread(target=closeBlinds).start()
 
 
 def setup():
@@ -120,10 +112,15 @@ def setup():
     motorOpen = GPIO.PWM(motorOpenPin, 100)
     motorClose = GPIO.PWM(motorClosePin, 100)
 
+    motorOpen.stop()
+    motorClose.stop()
+
     # Close the blinds slowly
     motorClose.start(60)
     time.sleep(60)
     motorClose.stop()
+
+    threading.Thread(target=checker).start()
 
 if __name__ == "__main__":
     setup()
